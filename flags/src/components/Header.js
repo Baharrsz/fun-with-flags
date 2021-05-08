07f866e92eee108787a631893441dd9ff8b19ad3
Bahar Raiszadeh <Bahar@Bahars-MBP-2.vc.shawcable.net>
2021-05-08 16:45:17 -0700

    Restyled the header.

--- flags/src/components/Header.js
+++ flags/src/components/Header.js
@@ -4,21 +4,28 @@ export default function Header() {
   return (
     <div className="header">
       <h1 className="header__title">
-        <span className="header__title__emphasis">Fun</span> With Flags
+        <span className="header__title header__title--fun">Fun </span>
+        <span className="header__title header__title--with">With </span>
+        <span className="header__title header__title--flags">Flags </span>
       </h1>
       <h3 className="header__subtitle">
-        Guess which country the flag represents
+        Guess which country the flag represents!
       </h3>
-      <p className="header__instructions">
-        You have 100 points at each stage and will lose 10 points for every
-        wrong guess. You'll go to the next level when you guess right.
-      </p>
-      <p className="header__instructions">
-        If you are sure of your guess, yet it's not correct, it might be because
-        the official name is different, or that the answer is another country
-        which shares the same flag.
-      </p>
-      <p className="header__instructions">Negative scores are also possible.</p>
+
+      <div className="header__instructions">
+        <p className="header__instruction">
+          You have 100 points at each stage and will lose 10 points for every
+          wrong guess. You'll go to the next level when you guess right.
+        </p>
+        <p className="header__instruction">
+          If you are sure of your guess, yet it's not correct, it might be
+          because the official name is different, or that the answer is another
+          country which shares the same flag.
+        </p>
+        <p className="header__instruction">
+          Negative scores are also possible.
+        </p>
+      </div>
     </div>
   );
 }
